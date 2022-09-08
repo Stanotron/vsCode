20292b4d4e798a517d22b4ed7251a33ecc64ca85
STANOTRON <jaintushar8864@gmail.com>
2022-09-09 00:18:32 +0530

    init

---
+++ c++/cps/ll.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(i,a,b) for(int i = a; i < b; i++)
+#define pb push_back
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+struct node{
+    int data;
+    node* next;
+};
+
+node* start = NULL;
+
+void insert(int val){
+    node* p = new node();
+    p->data = val;
+    if(start==NULL){
+        start = p;
+        return;
+    }
+    node* temp = start;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = p;
+
+}
+
+void display(){
+    node * temp = start;
+    while(temp!=NULL){
+        cout<<"->"<<temp->data;
+        temp=temp->next;
+    }
+}
+
+void invert(){
+    node* t = start;
+    node *t2 = start;
+    while(t->next->next!=NULL){
+        t = t->next;
+    }
+    start = t->next;
+    t->next = NULL;
+    start->next = t2;
+    cout<<endl;
+    display();
+}
+
+void checkpalindrome(){
+    stack <int> s;
+    int x=1;
+    node *t,*t2 = start;
+    while(t!=NULL){
+        s.push(t->data);
+        t = t->next;
+    }
+    while(s.empty()){
+        int x = s.top();
+        if(x==t2->data){
+            s.pop();
+            t2 = t2->next;
+        }
+    }
+    if(s.empty()) cout<<"true";
+    else cout<<"false";
+}
+
+using namespace std;
+int main()
+{
+    fast_io;
+    ll t;
+    t = 1;
+    // cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x;
+        fr(i,0,x){
+            cin>>y;
+            insert(y);
+        }
+    }
+    // display();
+    // invert();
+    checkpalindrome();
+    return 0;
+}
---
+++ c++/dsa/tempCodeRunnerFile.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node* link;
+};
+node *start = NULL;
+void push(int d){
+    node *p1 = new node();
+    p1->data = d;
+    if(start == NULL) {
+        start = p1;
+        return;
+    }
+    node *p2 = start;
+    while(p2->link!=NULL){
+           p2 = p2->link;           
+    }
+    p2->link = p1;
+}
+void display(){
+    while(start!=NULL){
+        cout<<"->"<<start->data;
+        start=start->link;
+    }
+}
+int main()
+{
+    int n;cin>>n;
+    int d;
+    for (int i=0;i<n;i++) {
+        cin>>d;
+        push(d);
+    }display();
+	return 0;
+}
