5ef1ba98ee86575ab1d52bdf39bb28ed100a754e
Stanotron <jaintushar8864@gmail.com>
2022-09-28 15:04:46 +0530

    init

--- python/comp_prof_skill/programs.py
+++
@@ -1,9 +0,0 @@
-i = int(input())
-l = []
-for n in range(1, i+1):
-    a = input('enter {} First name: '.format(n))
-    b = input('enter {} Second name: '.format(n))
-    l.append(a+" %s" % b)
-
-for i in l:
-    print("Good morning ", i)
