546010ede7ee62be13f35e0bb4c5e6da392466a8
Stanotron <jaintushar8864@gmail.com>
2022-11-01 03:11:08 +0530

    init

--- c++/hackerEarth/kjump.cpp
+++ c++/hackerEarth/kjump.cpp
@@ -1,19 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int tempjump(vector<int> A, int i, int j,int k){
+    long long t2jump = 1;
+    for(j;j<A.size();j++){
+        if(A[j]-A[i]>=k){
+            i=j;
+            ++t2jump;
+        } 
+    }
+    return t2jump;
+}
+
 int kjump (vector<int> A, int K) {
    long long i=0, jump=1, size = A.size();
-   while(i<A.size()){
-    long long temp = 1, j;
-    for(j = 1; j<size; j++){
-        if(A[j]-A[i]>=K){
-            i = j;
-            ++temp;
+    long long tjump;
+    while(i<size){
+        for(long long j = i+1; j<size; j++){
+            long long t2jump = tempjump(A,i,j,K);
+            if(t2jump>tjump) tjump=t2jump;
         }
-        if(jump<temp) jump = temp;
+        if(tjump>jump) jump=tjump;
+        i++;
     }
-   }
-   return jump;
+    return jump;
 }
 
 int main() {
