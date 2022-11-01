fe4b232e824f7213cb70927a22ddf7389940ac55
Stanotron <jaintushar8864@gmail.com>
2022-11-01 15:33:26 +0530

    init

--- c++/hackerEarth/kjump.cpp
+++ c++/hackerEarth/kjump.cpp
@@ -1,29 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int tempjump(vector<int> A, int i, int j,int k){
-    long long t2jump = 1;
-    for(j;j<A.size();j++){
-        if(A[j]-A[i]>=k){
-            i=j;
-            ++t2jump;
-        } 
-    }
-    return t2jump;
-}
-
 int kjump (vector<int> A, int K) {
-   long long i=0, jump=1, size = A.size();
-    long long tjump;
-    while(i<size){
-        for(long long j = i+1; j<size; j++){
-            long long t2jump = tempjump(A,i,j,K);
-            if(t2jump>tjump) tjump=t2jump;
-        }
-        if(tjump>jump) jump=tjump;
-        i++;
-    }
-    return jump;
+    
 }
 
 int main() {
---
+++ c++/hackerEarth/theamazingrace.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+    
+#define fr(i,a,b) for(int i = a; i < b; i++)
+#define pb push_back
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+    
+using namespace std;
+int main()
+{
+    fast_io;
+    ll t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n;
+        vector<int> d1;
+        fr(i,0,n){
+            cin>>x;
+            d1.push_back(x);
+        }
+        
+    }
+    return 0;
+}
