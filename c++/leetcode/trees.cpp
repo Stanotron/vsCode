3ba6a5ef115b7bff974796ed0a25d56606f3829b
Stanotron <jaintushar8864@gmail.com>
2022-12-22 16:45:15 +0530

    init

--- c++/leetcode/trees.cpp
+++ c++/leetcode/trees.cpp
@@ -9,6 +9,15 @@ using namespace std;
 
 //same tree
 
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
@@ -21,6 +30,7 @@ public:
         else return false;
     }
 };
+
 
 
 int main()
