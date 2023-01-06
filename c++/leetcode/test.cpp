14430d0b7295e9b398c8e84cc30497b162807d27
Stanotron <jaintushar8864@gmail.com>
2023-01-06 18:16:17 +0530

    init

--- c++/leetcode/test.cpp
+++ c++/leetcode/test.cpp
@@ -7,6 +7,24 @@
     
 using namespace std;
 
+class Solution {
+public:
+    int diam = 0;
+    int dfs(TreeNode* root){
+        if(!root) return -1;
+        int left = -1, right= -1;
+        if(root->left) left = dfs(root->left);
+        if(root->right) right = dfs(root->right);
+        diam = max(diam,2+left+right);
+        return 1+max(left,right);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root) return 0;
+        dfs(root);
+        return diam;
+    }
+};
+
 int main()
 {
     fast_io;
