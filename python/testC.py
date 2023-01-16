1a433a58dbcf73cc6c367987f4e5f4351a851a18
Stanotron <jaintushar8864@gmail.com>
2023-01-16 11:09:06 +0530

    init

--- python/testC.py
+++ python/testC.py
@@ -1,11 +1,23 @@
-import socket
+import numpy
+import random
+import statistics
 
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+data = []
+for i in range (10):
+    a = random.randint(1,50)
+    data.append(a)
 
-host = socket.gethostname()
-port = 8000
+x = numpy.mean(data)
+print ("mean is:  " + str(x))
 
-msg = "HELLO".encode()
+y = numpy.median(data)
+print("median is: "+ str(y))
 
-s.sendto(msg,(host,port))
+z = statistics.mode(data)
+print("mode is: "+str(z))
 
+a = numpy.std(data)
+print("standard devaition is: "+str(a))
+
+b = numpy.var(data)
+print("variance is: "+str(b))
