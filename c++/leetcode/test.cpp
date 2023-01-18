97aa14941cc5da25d4ba23c6b2ad844dd273f1e7
Stanotron <jaintushar8864@gmail.com>
2023-01-18 15:37:00 +0530

    init

--- c++/leetcode/test.cpp
+++ c++/leetcode/test.cpp
@@ -9,23 +9,33 @@ using namespace std;
 
 class Solution {
 public:
-
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> vvi;
-        if(nums.size()==0) return {{}};
-        if(nums.size()==1){
-            return {nums};
-        }
-        for(int i = 0; i<nums.size(); i++){
-            int x = nums[0];
-            nums.erase(nums.begin());
-            vector<vector<int>> v = permute(nums);
-            for(auto y : v){
-                y.push_back(x);
-                vvi.push_back(y);
+    bool ispalindrome(string str){
+        string x = str;
+        reverse(x.begin(),x.end());
+        return (x==str);
+    }
+    vector<vector<string>> vvi;
+    void dfs(string s, int pos, vector<string> & res){
+        if(pos==s.size()){
+            vvi.push_back(res);
+            return;
+        } 
+        string str;
+        int size = 1;
+        for(int i = pos; i<s.size();i++){
+            str = s.substr(pos, size);
+            if(ispalindrome(str)){
+                res.push_back(str);
+                dfs(s,pos+size,res);
+                res.pop_back();
             }
-            nums.push_back(x);
-        }
+            size++;
+        }   
+    }
+    vector<vector<string>> partition(string s) {
+        if(s.size()==1) return {{s}};
+        vector<string> res;
+        dfs(s,0,res);
         return vvi;
     }
 };
--- python/testS.py
+++ python/testS.py
@@ -1,14 +1,54 @@
-import socket
-import time
 
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+def gcd(a, b):
+	if b==0:
+		return a
+	return gcd(b, a%b)
 
-host = socket.gethostname()
-port = 8000
+def Pour(toJugCap, fromJugCap, d):
 
-s.bind((host,port))
+	fromJug = fromJugCap
+	toJug = 0
 
-while True:
-    data,addr = s.recvfrom(1024)
-    print(data)
+	step = 1
+	while ((fromJug is not d) and (toJug is not d)):
+
+		temp = min(fromJug, toJugCap-toJug)
+
+		toJug = toJug + temp
+		fromJug = fromJug - temp
+
+		step = step + 1
+		if ((fromJug == d) or (toJug == d)):
+			break
+
+		if fromJug == 0:
+			fromJug = fromJugCap
+			step = step + 1
+
+		if toJug == toJugCap:
+			toJug = 0
+			step = step + 1
+			
+	return step
+
+def minSteps(n, m, d):
+	if m> n:
+		temp = m
+		m = n
+		n = temp
+		
+	if (d%(gcd(n,m)) is not 0):
+		return -1
+	
+	return(min(Pour(n,m,d), Pour(m,n,d)))
+
+if __name__ == '__main__':
+
+	n = 3
+	m = 5
+	d = 4
+
+	print('Minimum number of steps required is',
+							minSteps(n, m, d))
+	
 
