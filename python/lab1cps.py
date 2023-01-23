4bf550f3216ea4a6d8738d73ef09f7fd42f62844
Stanotron <jaintushar8864@gmail.com>
2023-01-23 11:00:00 +0530

    init

--- python/lab1cps.py
+++ python/lab1cps.py
@@ -1,23 +1,30 @@
-import numpy
-import random
-import statistics
+# import numpy
+# import random
+# import statistics
 
-data = []
-for i in range (10):
-    a = random.randint(1,50)
-    data.append(a)
+# data = []
+# for i in range (10):
+#     a = random.randint(1,50)
+#     data.append(a)
 
-x = numpy.mean(data)
-print ("mean is:  " + str(x))
+# x = numpy.mean(data)
+# print ("mean is:  " + str(x))
 
-y = numpy.median(data)
-print("median is: "+ str(y))
+# y = numpy.median(data)
+# print("median is: "+ str(y))
 
-z = statistics.mode(data)
-print("mode is: "+str(z))
+# z = statistics.mode(data)
+# print("mode is: "+str(z))
 
-a = numpy.std(data)
-print("standard devaition is: "+str(a))
+# a = numpy.std(data)
+# print("standard devaition is: "+str(a))
 
-b = numpy.var(data)
-print("variance is: "+str(b))
+# b = numpy.var(data)
+# print("variance is: "+str(b))
+
+
+numbers = [1.11, 2.413, 3.415, 4.00, 5.857, 1.100]
+
+print("sums is :",sum(numbers))
+
+print("integer type conversion :" , int(sum(numbers)))
