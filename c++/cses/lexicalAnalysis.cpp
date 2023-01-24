2083d6165f91df0530db7a55327f990975ef0bc7
Stanotron <jaintushar8864@gmail.com>
2023-01-24 16:36:49 +0530

    init

---
+++ c++/cses/lexicalAnalysis.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+    
+#define fr(i,a,b) for(int i = a; i < b; i++)
+#define pb push_back
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+using namespace std;
+int main()
+{
+    fast_io;
+    ll t;
+    t = 1;
+    set<string> si {
+    "for","while","do","int","string","char","long","long","vector","set","map","if","else","float"};
+    int keys = 0, identifier = 0;
+    string temp ; 
+    // cin>>t;
+    while(t--){
+        string in = "for(int i = 0; i < 10; i++)";
+        for(auto x : in){
+            if((int(x)>64 && int(x)<91) || (int(x)>96 && int(x)<123)){
+                temp.push_back(x);
+            }
+            else{
+                if(temp.empty()) continue;
+                if(si.find(temp)==si.end()) identifier++;
+                else keys++;
+                temp.clear(); 
+            }
+        }
+    }
+    cout<<keys<<" "<<identifier;
+    return 0;
+}
