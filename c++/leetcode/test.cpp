a43a26688fd278681716d0f97dbecdf725ec4687
Stanotron <jaintushar8864@gmail.com>
2023-01-26 14:32:02 +0530

    init

--- c++/leetcode/test.cpp
+++ c++/leetcode/test.cpp
@@ -9,93 +9,49 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> vii;
-
-    bool checkPO(int i, int j, vector<vector<int>>& heights){
-        if(j==0 || i==0) return true;
-        int temp = heights[i][j];
-
-        heights[i][j] = INT_MAX; 
-        
-        if(temp>=heights[i-1][j]){
-            if(checkPO(i-1,j,heights)){
-                heights[i][j] = temp;
-                return true;
+    int valid = true;
+    vector<pair<int,int>> vii;
+    void dfs(int i, int j, vector<vector<char>>& board){
+        if(i==board.size()-1 || j==board[0].size()-1 || i==0 || j==0){
+            if(board[i][j]=='O'){
+                valid = false;
             } 
+            return;
         }
-        if(temp>=heights[i][j-1]){
-            if(checkPO(i,j-1,heights)){
-                heights[i][j] = temp;
 
-                return true;
-            } 
-        }  
-        
-        if(j<heights[0].size()-1 && temp>=heights[i][j+1]){
-            if(checkPO(i,j+1,heights)){
-                heights[i][j] = temp;
-                return true;
-            } 
+        if(board[i][j]=='O'){
+            cout<<i<<" "<<j<<endl;
+            board[i][j]='X';
+            vii.emplace_back(i,j);
+            if(valid) dfs(i,j+1,board);
+            if(valid) dfs(i,j-1,board);
+            if(valid) dfs(i+1,j,board);
+            if(valid) dfs(i-1,j,board);
         }
-        
-        if(i<heights.size()-1 && temp>=heights[i+1][j]){
-            if(checkPO(i+1,j,heights)){
-                heights[i][j] = temp;
-                return true;
-            }
-        }
-        heights[i][j] = temp;
-        return false;
-    }
-
-    bool checkAO(int i, int j, vector<vector<int>>& heights){
-        if(j==heights[0].size()-1 || i==heights.size()-1) return true;
-        
-        int temp = heights[i][j];
-        heights[i][j] = INT_MAX;
-        
-        if(temp>=heights[i+1][j]){
-           if(checkAO(i+1,j,heights)){
-                heights[i][j] = temp;
-                return true;
-           }  
+        if(!valid){
+            board[i][j] = 'O';
         } 
-
-        if(temp>=heights[i][j+1]){
-           if(checkAO(i,j+1,heights)){
-                heights[i][j] = temp;
-                return true;  
-           }  
-        }
-
-        if(i>0 && temp>=heights[i-1][j]){
-            if(checkAO(i-1,j,heights)){
-                heights[i][j] = temp;
-                return true;
-            } 
-        }
-        
-        if(j>0 && temp>=heights[i][j-1]){
-            if(checkAO(i,j-1,heights)){
-                heights[i][j] = temp;
-                return true;
-            } 
-        }
-        
-        heights[i][j] = temp;
-        return false;
+        return;   
     }
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        if(heights.size()==1 && heights[0].size()==1) return {{0,0}};
-        for(int i = 0; i<heights.size(); i++){
-            for(int j = 0; j<heights[0].size(); j++){
-                if(checkPO(i,j,heights) && checkAO(i,j,heights)){
-                    vii.push_back({i,j});
-                } 
+
+    void solve(vector<vector<char>>& board) {
+        if(board.size()==1 && board[0].size()==1) return;
+
+        for(int i = 1; i<board.size()-1; i++){
+            for(int j = 1; j<board[0].size()-1; j++){
+                if(board[i][j]=='O'){
+                    dfs(i,j,board);
+                    if(!valid && !vii.empty()){
+                        for(auto c : vii){
+                            board[c.first][c.second]='O';
+                        }
+                    }
+                    vii.clear();
+                    valid = true;
+                }  
             }
         }
-        // cout<<checkAO(0,1,heights);
-        return vii;
+        return;
     }
 };
 
@@ -113,187 +69,6 @@ int main()
     // cin>>t;
     while(t--){
         Solution sol;
-        sol.numIslands(v);
     }
     return 0;
-}
-
-
-#include<bits/stdc++.h>
-using namespace std;
-
-#define COMPUTER 1
-#define HUMAN 2
-
-#define SIDE 3 
-
-#define COMPUTERMOVE 'O'
-#define HUMANMOVE 'X'
-
-void showBoard(char board[][SIDE])
-{
-	printf("\n\n");
-	
-	printf("\t\t\t %c | %c | %c \n", board[0][0],
-							board[0][1], board[0][2]);
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t %c | %c | %c \n", board[1][0],
-							board[1][1], board[1][2]);
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t %c | %c | %c \n\n", board[2][0],
-							board[2][1], board[2][2]);
-
-	return;
-}
-
-void showInstructions()
-{
-	printf("\t\t\t Tic-Tac-Toe\n\n");
-	printf("Choose a cell numbered from 1 to 9 as below"
-			" and play\n\n");
-	
-	printf("\t\t\t 1 | 2 | 3 \n");
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t 4 | 5 | 6 \n");
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t 7 | 8 | 9 \n\n");
-	
-	printf("-\t-\t-\t-\t-\t-\t-\t-\t-\t-\n\n");
-
-	return;
-}
-
-
-void initialise(char board[][SIDE], int moves[])
-{
-	srand(time(NULL));
-	
-	for (int i=0; i<SIDE; i++)
-	{
-		for (int j=0; j<SIDE; j++)
-			board[i][j] = ' ';
-	}
-	
-	for (int i=0; i<SIDE*SIDE; i++)
-		moves[i] = i;
-
-	random_shuffle(moves, moves + SIDE*SIDE);
-	
-	return;
-}
-
-void declareWinner(int whoseTurn)
-{
-	if (whoseTurn == COMPUTER)
-		printf("COMPUTER has won\n");
-	else
-		printf("HUMAN has won\n");
-	return;
-}
-
-bool rowCrossed(char board[][SIDE])
-{
-	for (int i=0; i<SIDE; i++)
-	{
-		if (board[i][0] == board[i][1] &&
-			board[i][1] == board[i][2] &&
-			board[i][0] != ' ')
-			return (true);
-	}
-	return(false);
-}
-
-bool columnCrossed(char board[][SIDE])
-{
-	for (int i=0; i<SIDE; i++)
-	{
-		if (board[0][i] == board[1][i] &&
-			board[1][i] == board[2][i] &&
-			board[0][i] != ' ')
-			return (true);
-	}
-	return(false);
-}
-
-bool diagonalCrossed(char board[][SIDE])
-{
-	if (board[0][0] == board[1][1] &&
-		board[1][1] == board[2][2] &&
-		board[0][0] != ' ')
-		return(true);
-		
-	if (board[0][2] == board[1][1] &&
-		board[1][1] == board[2][0] &&
-		board[0][2] != ' ')
-		return(true);
-
-	return(false);
-}
-
-bool gameOver(char board[][SIDE])
-{
-	return(rowCrossed(board) || columnCrossed(board)
-			|| diagonalCrossed(board) );
-}
-
-void playTicTacToe(int whoseTurn)
-{
-	char board[SIDE][SIDE];
-	
-	int moves[SIDE*SIDE];
-	
-	initialise(board, moves);
-	
-	showInstructions();
-	
-	int moveIndex = 0, x, y;
-	
-	while (gameOver(board) == false &&
-			moveIndex != SIDE*SIDE)
-	{
-		if (whoseTurn == COMPUTER)
-		{
-			x = moves[moveIndex] / SIDE;
-			y = moves[moveIndex] % SIDE;
-			board[x][y] = COMPUTERMOVE;
-			printf("COMPUTER has put a %c in cell %d\n",
-					COMPUTERMOVE, moves[moveIndex]+1);
-			showBoard(board);
-			moveIndex ++;
-			whoseTurn = HUMAN;
-		}
-		
-		else if (whoseTurn == HUMAN)
-		{
-			x = moves[moveIndex] / SIDE;
-			y = moves[moveIndex] % SIDE;
-			board[x][y] = HUMANMOVE;
-			printf ("HUMAN has put a %c in cell %d\n",
-					HUMANMOVE, moves[moveIndex]+1);
-			showBoard(board);
-			moveIndex ++;
-			whoseTurn = COMPUTER;
-		}
-	}
-
-	if (gameOver(board) == false &&
-			moveIndex == SIDE * SIDE)
-		printf("It's a draw\n");
-	else
-	{
-		if (whoseTurn == COMPUTER)
-			whoseTurn = HUMAN;
-		else if (whoseTurn == HUMAN)
-			whoseTurn = COMPUTER;
-		
-		declareWinner(whoseTurn);
-	}
-	return;
-}
-
-int main()
-{
-	playTicTacToe(COMPUTER);
-	
-	return (0);
 }
