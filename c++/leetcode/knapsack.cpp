37139b3d6926c27dce837ee3ccab3de3897ffb2d
Stanotron <jaintushar8864@gmail.com>
2023-02-08 23:52:52 +0530

    init

--- c++/leetcode/knapsack.cpp
+++ c++/leetcode/knapsack.cpp
@@ -7,14 +7,18 @@
     
 using namespace std;
 
+int dp[102][1002];
+
 int knapsack(vector<int> & wt, vector<int> & val , int w, int n ){
     if(n==0 || w==0 ) return 0;
     
+    if(dp[n][w]!=-1) return dp[n][w];
+    
     if(wt[n-1] <= w){
-        return max((val[n-1] + knapsack(wt,val,w-wt[n-1],n-1)), knapsack(wt,val,w,n-1));
+        return dp[n][w] = max((val[n-1] + knapsack(wt,val,w-wt[n-1],n-1)), knapsack(wt,val,w,n-1));
     }
     
-    else if(wt[n-1]>w) return knapsack(wt,val,w,n-1);
+    else if(wt[n-1]>w) return dp[n][w] = knapsack(wt,val,w,n-1);
 }
 
 int main()
@@ -23,6 +27,7 @@ int main()
     ll t;
     t = 1;
     // cin>>t;
+    memset(dp,-1,sizeof(dp));
     while(t--){
         vector<int> wt {10,20,30}, val{60,100,120};
         int w = 50;
