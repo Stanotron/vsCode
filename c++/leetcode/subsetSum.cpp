08f00f3ab289071c68ec0f2d06679a5e7e460b11
Stanotron <jaintushar8864@gmail.com>
2023-02-16 00:39:08 +0530

    init

--- c++/leetcode/subsetSum.cpp
+++ c++/leetcode/subsetSum.cpp
@@ -23,18 +23,34 @@ using namespace std;
 //     return sol[size][sum];
 // }
 
-int dp[102][1002];
+int subset(int arr[], int sum, int size){
+    int sol[size+1][sum+1];
+    fr(i,0,size+1){
+        fr(j,0,sum+1){
+            if(i==0) sol[i][j] = 0;
+            if(j==0) sol[i][j] = 1;
 
-int subset(int arr[], int sum, int n){
-    if(sum==0 || n==0) return 0;
-
-    if(dp[n][sum]!=-1) return dp[n][sum];
-    
-    if(arr[n-1]<=sum){
-        return dp[n][sum] = max(subset(arr,sum,n-1), arr[n-1] + subset(arr,sum-arr[n-1],n-1));
+            if(arr[i-1]<j){
+                sol[i][j] = sol[i-1][j] + sol[i-1][j - arr[i-1]];
+            }
+            else sol[i][j] = sol[i-1][j];
+        }
     }
-    else return dp[n][sum] = subset(arr,sum,n-1);
+    return sol[size][sum];
 }
+
+// int dp[102][1002];
+
+// int subset(int arr[], int sum, int n){
+//     if(sum==0 || n==0) return 0;
+
+//     if(dp[n][sum]!=-1) return dp[n][sum];
+    
+//     if(arr[n-1]<=sum){
+//         return dp[n][sum] = max(subset(arr,sum,n-1), arr[n-1] + subset(arr,sum-arr[n-1],n-1));
+//     }
+//     else return dp[n][sum] = subset(arr,sum,n-1);
+// }
 
 int main()
 {
@@ -43,11 +59,9 @@ int main()
     t = 1;
     // cin>>t;
     while(t--){
-        memset(dp,-1,sizeof(dp));
-        int arr[5] {2,3,7,8,10}, sum = 11;
-        int sol = subset(arr,sum,5); 
-        cout<<(sol == 11)<<endl;
-        cout<<sol;
+        // memset(dp,-1,sizeof(dp));
+        int arr[5] {2,3,5,8,10}, sum = 10;
+        cout<<subset(arr,sum,5);
     }
     return 0;
 }
