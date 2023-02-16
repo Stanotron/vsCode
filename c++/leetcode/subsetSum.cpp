ab1bb8ebaf0d5d3bed889b13d21bf1fafd0e1c74
Stanotron <jaintushar8864@gmail.com>
2023-02-16 22:22:46 +0530

    init

--- c++/leetcode/subsetSum.cpp
+++ c++/leetcode/subsetSum.cpp
@@ -7,23 +7,41 @@
     
 using namespace std;
 
-// bool subset(int arr[], int sum, int size){
-//     int sol[size+1][sum+1];
-//     fr(i,0,size+1){
-//         fr(j,0,sum+1){
-//             if(i==0) sol[i][j] = false;
-//             if(j==0) sol[i][j] = true;
+//iterative approach
 
-//             if(arr[i-1]<j){
-//                 sol[i][j] = (sol[i-1][j] || sol[i-1][j - arr[i-1]]);
-//             }
-//             else sol[i][j] = sol[i-1][j];
-//         }
-//     }
-//     return sol[size][sum];
-// }
+bool subset(int arr[], int sum, int size){
+    int sol[size+1][sum+1];
+    fr(i,0,size+1){
+        fr(j,0,sum+1){
+            if(i==0) sol[i][j] = false;
+            if(j==0) sol[i][j] = true;
 
-int subset(int arr[], int sum, int size){
+            if(arr[i-1]<j){
+                sol[i][j] = (sol[i-1][j] || sol[i-1][j - arr[i-1]]);
+            }
+            else sol[i][j] = sol[i-1][j];
+        }
+    }
+    return sol[size][sum];
+}
+
+// recursion and memoization
+
+int dp[102][1002];
+int subset2(int arr[], int sum, int n){
+    if(sum==0 || n==0) return 0;
+
+    if(dp[n][sum]!=-1) return dp[n][sum];
+    
+    if(arr[n-1]<=sum){
+        return dp[n][sum] = max(subset2(arr,sum,n-1), arr[n-1] + subset2(arr,sum-arr[n-1],n-1));
+    }
+    else return dp[n][sum] = subset2(arr,sum,n-1);
+}
+
+//number of subsets equal to sum
+
+int subset3(int arr[], int sum, int size){
     int sol[size+1][sum+1];
     fr(k,0,sum+1){
         sol[0][k] = 0;
@@ -38,18 +56,7 @@ int subset(int arr[], int sum, int size){
     return sol[size][sum];
 }
 
-// int dp[102][1002];
 
-// int subset(int arr[], int sum, int n){
-//     if(sum==0 || n==0) return 0;
-
-//     if(dp[n][sum]!=-1) return dp[n][sum];
-    
-//     if(arr[n-1]<=sum){
-//         return dp[n][sum] = max(subset(arr,sum,n-1), arr[n-1] + subset(arr,sum-arr[n-1],n-1));
-//     }
-//     else return dp[n][sum] = subset(arr,sum,n-1);
-// }
 
 int main()
 {
@@ -58,9 +65,9 @@ int main()
     t = 1;
     // cin>>t;
     while(t--){
-        // memset(dp,-1,sizeof(dp));
+        memset(dp,-1,sizeof(dp));
         int arr[5] = {2,3,5,8,10}, sum = 10;
-        cout<<subset(arr,sum,5);
+        
     }
     return 0;
 }
