8f46cf98867cc5102c318c0cdef18b3504a0353b
Stanotron <jaintushar8864@gmail.com>
2023-03-21 11:56:21 +0530

    init

--- python/lab1cps.py
+++ python/lab1cps.py
@@ -23,8 +23,28 @@
 # print("variance is: "+str(b))
 
 
-numbers = [1.11, 2.413, 3.415, 4.00, 5.857, 1.100]
+# numbers = [1.11, 2.413, 3.415, 4.00, 5.857, 1.100]
 
-print("sums is :",sum(numbers))
+# print("sums is :",sum(numbers))
 
-print("integer type conversion :" , int(sum(numbers)))
+# print("integer type conversion :" , int(sum(numbers)))
+
+import math
+
+def minimax (curDepth, nodeIndex, maxTurn, scores,targetDepth) :
+    if(curDepth==targetDepth):
+        return scores[nodeIndex]
+    if(maxTurn):
+        return max(minimax(curDepth+1,
+nodeIndex*2,False,scores,targetDepth),minimax(curDepth+1,
+nodeIndex*2+1,False,scores,targetDepth))
+    else:
+        return min(minimax(curDepth+1,
+nodeIndex*2,True,scores,targetDepth),minimax(curDepth+1,
+nodeIndex*2+1,True,scores,targetDepth))
+        
+scores=[-1,4,2,6,-3,-5,0,7]
+treeDepth=math.log(len(scores),2)
+
+print("Optimal value is : ",end=" ")
+print(minimax(0,0,True,scores,treeDepth))
