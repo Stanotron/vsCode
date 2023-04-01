c5117ade30b12797a94ca07ca197831e4eb9ff41
Stanotron <jaintushar8864@gmail.com>
2023-04-01 18:20:32 +0530

    init

--- c++/leetcode/longestpalindrome.cpp
+++ c++/leetcode/longestpalindrome.cpp
@@ -11,18 +11,34 @@ string palindrome(string s){
     string a = s,d;
     reverse(a.begin(), a.end());
     int size = s.length(), dp[size+1][size+1];
+    int max = 0,x=0,y=0;
     fr(i,0,size+1){
         fr(j,0,size+1){
             if(i==0 || j==0) dp[i][j] = 0;
 
             else if(s[i]==a[j]){
-                d.push_back(s[i]);
                 dp[i][j] = 1+dp[i-1][j-1];
             }
-            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+
+            else dp[i][j] = 0;
+
+            if(dp[i][j]>max){
+                x = i;
+                y = j;
+                max = dp[i][j];
+            } 
         }
     }
-    cout<<size<<" "<<dp[size][size]<<endl;
+    while(x>=0 && y>=0){
+        if(dp[x][y]==1+dp[x-1][y-1]){
+            d.push_back(s[x]);
+            x-=1;
+            y-=1;
+        } 
+        else if(dp[x-1][y]>=dp[x][y-1]) x -=1;
+        
+        else y-=1;
+    }
     return d;
 }
 
