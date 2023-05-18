9fc1520e2ba90c877e42040a25e589941d91404e
Stanotron <jaintushar8864@gmail.com>
2023-05-18 22:01:02 +0530

    init

--- c++/extras/cps.cpp
+++ c++/extras/cps.cpp
@@ -6,45 +6,24 @@
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
     
 using namespace std;
-
-int q1(long s, map<long,long> mii){
-    long temp;
-    if(s%2!=0) temp = (s/2)+1;
-    else temp = s/2; 
-    for(auto &x : mii){
-        if(x.second>=temp){
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int q2(long s, vector<long> vi){
-    
-    return 0;
-}
-
 int main()
 {
     fast_io;
     ll t;
-    t = 2;
+    t = 1;
     // cin>>t;
     while(t--){
-        //Q1
-        long s,temp;
-        map<long,long> mii;
-        vector<long> vi;
-        cin>>s;
-        fr(i,0,s){
-            cin>>temp;
-            // vi.push_back(temp);
-            mii[temp] = mii[temp]+1;
-        }
-        for(auto &x : mii) cout<<x.first<<" "<<x.second<<endl;
-        cout<<q1(s,mii)<<endl;
-        cout<<q2(s,vi);
+        string in;
+        cin>>in;
+        for (int i = 0; i < in.size(); i++)
+        {
+            if(in[i]!='a'){
+                cout<<in[i];
+                cout<<i<<endl;
+            }
+
+        } 
+        
     }
     return 0;
 }
-
--- c++/extras/srp.cpp
+++ c++/extras/srp.cpp
@@ -1,13 +1,47 @@
 #include <bits/stdc++.h>
-    
-#define fr(i,a,b) for(int i = a; i < b; i++)
-#define pb push_back
+
 #define ll long long
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
     
 using namespace std;
 
-
+bool srp (string &in, vector<pair<char,char>> &terminals, vector<string> &rules){
+    int inpsize = in.size();
+    string stack;
+    stack.push_back('$');
+    for(int i = 0; i<inpsize; i++){
+        char cur = in[i];
+        if(cur>='a' && cur<='z'){
+            for(auto x : terminals){
+                if(cur==x.second){
+                    stack.push_back(x.first);
+                }
+            }
+        }
+        else{
+            stack.push_back(cur);
+        }
+        cout<<stack<<endl;
+        for(auto r : rules){
+            int rulesize = r.size(), stacksize = stack.size()-1,count = 0;
+            if(rulesize-3<=stacksize && rulesize-3>1){
+                int t = stacksize;
+                for(int a = 3; a<rulesize; a++){
+                    if(r[a]==stack[t]){
+                        count++;
+                        t--;
+                    }
+                }
+                if(count==(rulesize-3)){
+                    stack.resize(stacksize+1-count);
+                    stack.push_back(r[0]);
+                    cout<<stack<<endl;
+                }
+            }
+        }
+    }
+    return stack.size()==2;
+}
 
 int main()
 {
@@ -16,7 +50,19 @@ int main()
     t = 1;
     // cin>>t;
     while(t--){
-        
+        vector<pair<char,char>> terminals; int pos = -1;
+        vector<string> vvs {"E->E+E", "E->E*E" , "E->d"};
+        for (auto x : vvs){
+            for(int i = 0; i<x.size(); i++){
+                if(x[i]>='a' && x[i]<='z'){
+                    terminals.push_back(make_pair(x[0],x[i]));
+                } 
+            }
+        }
+        string input;
+        cin>>input;
+        if(srp(input,terminals,vvs)) cout<<"string is parsed successfully";
+        else cout<<"string cannot be parsed";
     }
     return 0;
 }
