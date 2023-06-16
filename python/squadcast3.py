6b64d18264888dfa37850e3f04b4c74f3f116e98
Stanotron <jaintushar8864@gmail.com>
2023-06-16 23:35:53 +0530

    init

--- python/squadcast3.py
+++ python/squadcast3.py
@@ -1,4 +1,4 @@
 import re
-s = "v=spf1 ip4:40.113.200.201 ip6:2001:db8:85a3:8d3:1319:8a2e:370:7348 include:thirdpartydomain.com ~all"
+s = "v=sp ip4:40.113.200.201 ip6:2001:db8:85a3:8d3:1319:8a2e:370:7348 include:thirdpartydomain.com ~all"
 ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', s )
 print(ip)               
