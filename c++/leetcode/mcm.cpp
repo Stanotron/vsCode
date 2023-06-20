4f2154c716d355a3a2174e19fa48f868d9fffe43
Stanotron <jaintushar8864@gmail.com>
2023-06-20 23:42:42 +0530

    init

--- c++/leetcode/mcm.cpp
+++ c++/leetcode/mcm.cpp
@@ -35,3 +35,30 @@ int main()
     }
     return 0;
 }
+
+
+
+class Solution {
+public:
+    int dp[301][301]; int size;
+    int solve(vector<int> &nums, int i, int j){
+        if(i>j) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        int ans = 0;
+        for(int k = i; k<=j; k++){
+            int temp = nums[i-1]*nums[k]*nums[j+1];
+            temp += solve(nums,i,k-1) + solve(nums,k+1,j);
+            ans = max(temp,ans);
+        }
+        return dp[i][j] = ans;
+    }
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        size = nums.size();
+        if(size == 3) return nums[1];
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,1,size-2);       
+    }
+};
