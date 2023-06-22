b9f0fa9b60a93e404bb7db9d2efc315a49dc1e9d
Stanotron <jaintushar8864@gmail.com>
2023-06-23 02:58:58 +0530

    init

--- c++/leetcode/subsetSum.cpp
+++ c++/leetcode/subsetSum.cpp
@@ -99,3 +99,22 @@ int main()
     }
     return 0;
 }
+
+
+
+
+class Solution {
+public:
+    int f(int m,int n,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        if(n==0 && m==0) return grid[0][0];
+        if(n<0 || m<0) return 1e9;
+        if(dp[m][n]!=-1) return dp[m][n];
+        return dp[m][n]= grid[m][n]+min(f(m-1,n,grid,dp),f(m,n-1,grid,dp));
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,grid,dp);
+    }
+};
